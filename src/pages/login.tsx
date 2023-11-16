d6b6cce8dcb4611123b54a52bfe8b57c16bcff01
scrod <xueyuqi@bytedance.com>
2023-11-16 09:47:46 +0800

    optimize : 优化mock数据写法&命名

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,7 +2,7 @@ import { useFormState,Form, Toast, Button,Typography } from '@douyinfe/semi-ui';
 import {useState} from 'react';
 
 export const Login = () => {
-    const [loginSuccess, setLoginSuccess] = useState(undefined);
+    const [loginErr, setLoginErr] = useState(undefined);
     const handleSubmit = async (values) => {
         const requestLogin= new Promise((resolve, reject) => {
             let isSuccessful = Math.random() >= 0.5;  // 随机成功或失败
@@ -10,22 +10,18 @@ export const Login = () => {
             if (isSuccessful) {
                 resolve({success:true});  // 成功，调用resolve
             } else {
-                resolve({msg:'密码错误',code:1001})
+                reject({msg:'密码错误',code:1001})
             }
         })
         await requestLogin.then((res)=>{
-            if(res.msg){
-                setLoginSuccess(res.msg)
-            } else {
-                Toast.success('登录成功')
-            }
-        })
+            Toast.success('登录成功')
+        }).catch(err=>setLoginErr(err))
     };
     const {Title,Text}=Typography;
     const ConfirmButton = () => {
         const formState = useFormState();
         return (<div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <Button disabled={!(formState.values.email && formState.values.password)||loginSuccess} theme='solid' style={{ width: '80%' }} htmlType='submit'>登录</Button>
+            <Button disabled={!(formState.values.email && formState.values.password)||loginErr} theme='solid' style={{ width: '80%' }} htmlType='submit'>登录</Button>
          </div>
         )
     };
@@ -33,14 +29,14 @@ export const Login = () => {
         <div>
             <Title>登录</Title>
             <Form onSubmit={values => handleSubmit(values)} onChange={()=>{
-                if(loginSuccess) {setLoginSuccess(undefined)}
+                if(loginErr) {setLoginErr(undefined)}
             }
             }>
                 {({ formState, values, formApi }) => (
                     <>
                         <Form.Input  field='email' label='邮箱' style={{ width: '80%' }} placeholder='请输入'></Form.Input>
                         <Form.Input field='password' mode="password" label='密码' style={{ width: '80%' }} placeholder='请输入6-8位密码'></Form.Input>
-                        {loginSuccess&& <Text type="danger">{loginSuccess}</Text>}
+                        {loginErr&& <Text type="danger">{loginErr.msg}</Text>}
                         <ConfirmButton/>
                     </>
                 )}
