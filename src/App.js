aa0dbd598a8bc84289f8b628b92bd7a4bb9eaec7
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2023-11-17 10:08:30 +0800

    添加访问方法

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Login} from './pages/login.tsx';
 import {Space} from "@douyinfe/semi-ui";
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import {Profile} from "./pages/profileTest.tsx";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile/>}/>
         </Routes>
       </Router>
   );
@@ -17,6 +19,7 @@ const Home = () => {
   return (
       <Space>
         <Link to="/login">登录</Link>
+        <Link to="/profile">个人</Link>
       </Space>
   );
 }
