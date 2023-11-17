f68e44b6ece1bc00c5bd3daf2aa87294f86e7c18
GaryZhangstu <tianrui.zhang.2101@stu.moonshotacademy.cn>
2023-11-17 11:09:32 +0800

    帖子

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import {Profile} from "./pages/profileTest.tsx";
 import {Feed} from "./pages/feed/feed.tsx";
 import {Notifications} from "./pages/notifications/notifications.tsx";
+import PushPost from "./pages/Post/PushPost";
+import detailPost from "./pages/Post/detailPost";
+import DetailPost from "./pages/Post/detailPost";
 
 const App = () => {
   return (
@@ -14,6 +17,8 @@ const App = () => {
           <Route path="/profile" element={<Profile/>}/>
             <Route path="/feed" element={<Feed/>}/>
             <Route path="/notifications" element={<Notifications/>}/>
+            <Route path="/pushpost" element={<PushPost/>}/>
+            <Route path="/detailedpost" element={<DetailPost/>}/>
         </Routes>
       </Router>
   );
@@ -28,6 +33,8 @@ const Home = () => {
         <Link to="/profile">个人</Link>
           <Link to="/feed">首页</Link>
           <Link to="/notifications">通知</Link>
+          <Link to="/detailedpost">帖子</Link>
+          <Link to="/pushpost">发布</Link>
       </Space>
           </>
   );
