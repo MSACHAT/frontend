fcfa625d121387faa401403c220e7fc54ab2c242
StanH123h <1735443634@qq.com>
2023-11-17 10:16:53 +0800

    Notif

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import {Login} from './pages/login.tsx';
 import {Feed} from "./pages/feed/feed.tsx";
 import {Notification} from "@douyinfe/semi-ui";
+import {Profile} from "./pages/profileTest.tsx";
+import {Notif} from "./components/notif.tsx";
+import {Notifications} from "./pages/notifications/notifications.tsx";
 function App() {
   return (
-    <Feed></Feed>
+    <Notifications></Notifications>
   );
 }
 
