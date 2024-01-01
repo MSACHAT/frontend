e3b3682dd130dfeb8b632615b8c0878a171437eb
StanH123h <1735443634@qq.com>
2024-01-01 23:35:00 +0800

    Notification Style Added Commit Influenced Domains: Page,MiddleWare

--- src/components/NotifComponent.jsx
+++ src/components/NotifComponent.jsx
@@ -1,51 +1,65 @@
 //思路:IF ELSE判断传入的Comment Type,根据不同的Comment Type返回不同的样式
 import { Avatar, Collapse, Typography, Image, Space } from '@douyinfe/semi-ui';
-import {
-  IconChevronDown,
-  IconChevronUp,
-  IconLikeHeart,
-  IconComment,
-} from '@douyinfe/semi-icons';
+import '../pages/notifications/notifStyle.scss';
 
 export const Notif = props => {
+  const likedYourNotif = '赞了你的动态';
   const { Text } = Typography;
-  const { messageType, userIcon, userName, sendTime, commentContent } = props;
+  const {
+    messageType,
+    userIcon,
+    userName,
+    sendTime,
+    commentContent,
+    isRead,
+    previewType,
+    previewString,
+  } = props;
+
+  const expandIconForComments =
+    previewType === 'image' ? (
+      <Space style={{ height: '100%', display: 'inline-flex', float: 'right' }}>
+        <Image
+          width={50}
+          height={50}
+          className={'notif-postpreview'}
+          src="https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/abstract.jpg"
+        />
+      </Space>
+    ) : (
+      <Space style={{ height: '100%', display: 'inline-flex', float: 'right' }}>
+        <Text
+          className={'notif-postpreview'}
+        >
+            {previewString}
+        </Text>
+      </Space>
+    );
   if (messageType === 'Comment') {
-    const expandIconForComments = (
-      <Space style={{ height: '100%', display: 'inline-flex', float: 'right' }}>
-        <IconChevronDown style={{ float: 'right', marginRight: 20 }} />
-        <Image
-          style={{}}
-          width={40}
-          height={40}
-          src="https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/abstract.jpg"
-        />
-      </Space>
-    );
-    const collapseIconForComments = (
-      <Space style={{ height: '100%', display: 'inline-flex', float: 'right' }}>
-        <IconChevronUp style={{ float: 'right', marginRight: 20 }} />
-        <Image
-          style={{}}
-          width={40}
-          height={40}
-          src="https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/abstract.jpg"
-        />
-      </Space>
-    );
     return (
-      <Collapse
-        expandIcon={expandIconForComments}
-        collapseIcon={collapseIconForComments}
-      >
+      <Collapse className={'notif-collapse'} expandIcon={expandIconForComments}>
         <Collapse.Panel
-          style={{ border: 'none' }}
           header={
             <div>
-              <Space align="center">
-                <IconComment size={'extra-large'}></IconComment>
-                <Avatar src={userIcon} />
-                <Text>{userName}</Text>
+              <Space align={'end'}>
+                <div className={isRead ? 'notif-read' : 'notif-newnotif'}></div>
+                <Avatar src={userIcon} className={'notif-avatar'} />
+                <Space align={screenLeft} vertical>
+                  <Text className={'notif-username'}>{userName}</Text>
+                  <Text
+                    ellipsis={{
+                      showTooltip: {
+                        opts: {
+                          content: '架构|Semi-inf|graph.cheet.relation',
+                          className: 'components-typography-demo',
+                        },
+                      },
+                    }}
+                    className={'notif-notifcontent'}
+                  >
+                    {commentContent}
+                  </Text>
+                </Space>
                 <Text type="quaternary" size={'small'}>
                   {sendTime}
                 </Text>
@@ -53,36 +67,35 @@ export const Notif = props => {
             </div>
           }
           itemKey="1"
-        >
-          <Text type={'secondary'} strong={true}>
-            {commentContent}
-          </Text>
-        </Collapse.Panel>
+        ></Collapse.Panel>
       </Collapse>
     );
   } else {
     const expandIconForLikes = (
       <Space style={{ height: '100%', float: 'right' }}>
         <Image
-          width={40}
-          height={40}
+          className={'notif-postpreview'}
+          width={50}
+          height={50}
           src="https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/abstract.jpg"
         />
       </Space>
     );
     return (
-      <Collapse expandIcon={expandIconForLikes}>
+      <Collapse expandIcon={expandIconForLikes} className={'notif-collapse'}>
         <Collapse.Panel
-          style={{ border: 'none' }}
           header={
             <div>
-              <Space align={'center'}>
-                <IconLikeHeart
-                  size={'extra-large'}
-                  style={{ color: 'red' }}
-                ></IconLikeHeart>
+              <Space align={'end'}>
+                <div
+                  className={isRead ? 'notif-read' : 'notif-newnotif'}
+                  style={{ backgroundColor: 'transparent' }}
+                ></div>
                 <Avatar src={userIcon} />
-                <Text size={'normal'}>{userName}</Text>
+                <Space vertical align={screenLeft}>
+                  <Text className={'notif-username'}>{userName}</Text>
+                  <Text className={'notif-notifcontent'}>{likedYourNotif}</Text>
+                </Space>
                 <Text type={'quaternary'} size={'small'}>
                   {sendTime}
                 </Text>
