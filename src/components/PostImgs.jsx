a0bfd83c38fa655d604f8fb8b2d3670f7372730f
scrod <xueyuqi@bytedance.com>
2024-01-02 21:06:06 +0800

    bugfix: disable lazy load

--- src/components/PostImgs.jsx
+++ src/components/PostImgs.jsx
@@ -11,6 +11,7 @@ export const PostImgs = ({ imgUrls }) => {
       className={`image-container ${imgUrls.length === 1 && 'container-1'} ${
         imgUrls.length < 5 && imgUrls.length > 1 && 'container-less-than-5'
       }`}
+      lazyLoad={false}
     >
       {imgUrls.map((x, index) => (
         <div className={'image-item'}>
