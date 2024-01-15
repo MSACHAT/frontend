4155c68733300de7fc77f65186110604565df223
scrod <xueyuqi@bytedance.com>
2024-01-15 20:09:46 +0800

    bugfix: 兼容生产环境

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,21 @@
+const webpack = require('webpack');
 const SemiWebpackPlugin = require('@douyinfe/semi-webpack-plugin').default;
 
 module.exports = {
-    webpack: {
-        plugins: [
-            new SemiWebpackPlugin({
-                theme: '@semi-bot/semi-theme-moonshot',
-                include: '~@semi-bot/semi-theme-moonshot/scss/local.scss',
-                name: '@semi-bot/semi-theme-moonshot'
-            })
-        ],
-    },
+  webpack: {
+    plugins: [
+      new SemiWebpackPlugin({
+        theme: '@semi-bot/semi-theme-moonshot',
+        include: '~@semi-bot/semi-theme-moonshot/scss/local.scss',
+        name: '@semi-bot/semi-theme-moonshot',
+      }),
+
+      // 定义环境变量
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(
+          process.env.NODE_ENV || 'development'
+        ),
+      }),
+    ],
+  },
 };
