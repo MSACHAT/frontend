b42487ad7611845775705c8c6541cb53fb064348
scrod <xueyuqi@bytedance.com>
2024-03-21 19:38:27 +0800

    t: 完成记录位置

--- src/pages/feed/FeedPage.tsx
+++ src/pages/feed/FeedPage.tsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, { useRef } from 'react';
 import { Layout, List, Badge } from '@douyinfe/semi-ui';
 import { Post } from '../../components/PostComponent.jsx';
 import { IconBellStroked } from '@douyinfe/semi-icons';
@@ -10,7 +10,7 @@ import BottomNavigationBar from '../../components/BottomNavigationBar';
 import { Link } from 'react-router-dom';
 import { PostModel } from '../../../types/post';
 const { Header } = Layout;
-export function NewNotif({ newNotifNums }:{newNotifNums:number}) {
+export function NewNotif({ newNotifNums }: { newNotifNums: number }) {
   if (newNotifNums > 0) {
     return (
       <Link to={'/notifications'} className={'feed-link'}>
@@ -36,8 +36,9 @@ export function Feed() {
   const [pageNum, setPageNum] = useState(0);
   const [newNotifNums, setNewNotifNums] = useState(0);
   const [loading, setLoading] = useState(false);
-  const [posts, setPosts] = useState< PostModel[]>([]);
+  const [posts, setPosts] = useState<PostModel[]>([]);
   const [totalPages, setTotalPages] = useState<number>(0);
+  const feedContentRef: any = useRef(null);
   function loadMoreData() {
     setLoading(true);
     GetData(pageNum, pageSize).then(result => {
@@ -47,29 +48,36 @@ export function Feed() {
       setPageNum(prevPageNum => prevPageNum + 1);
       setLoading(false);
       // 保存新状态到 sessionStorage
-      sessionStorage.setItem('feedState', JSON.stringify({
-        posts: newPosts,
-        totalPages: result.totalPages,
-        pageNum: pageNum + 1,
-        newNotifNums: result.newNotifCounts,
-      }));
+      sessionStorage.setItem(
+        'feedState',
+        JSON.stringify({
+          posts: newPosts,
+          totalPages: result.totalPages,
+          pageNum: pageNum + 1,
+          newNotifNums: result.newNotifCounts,
+        })
+      );
     });
   }
-
 
   // 组件加载时的副作用
   useEffect(() => {
     // 尝试从 sessionStorage 恢复状态
     const savedState = sessionStorage.getItem('feedState');
     if (savedState) {
-      console.log(
-          'restore'
-      )
-      const { posts, totalPages, pageNum, newNotifNums } = JSON.parse(savedState);
+      console.log('restore');
+      const { posts, totalPages, pageNum, newNotifNums } =
+        JSON.parse(savedState);
       setPosts(posts);
       setTotalPages(totalPages);
       setPageNum(pageNum);
       setNewNotifNums(newNotifNums);
+      const savedScrollPosition = sessionStorage.getItem('scrollPosition');
+      if (savedScrollPosition && feedContentRef.current) {
+        setTimeout(() => {
+          feedContentRef.current!.scrollTop = parseInt(savedScrollPosition);
+        }, 10);
+      }
     } else {
       // 如果没有保存的状态，就获取数据
       setLoading(true);
@@ -79,18 +87,40 @@ export function Feed() {
         setNewNotifNums(result.newNotifCounts);
         setLoading(false);
         // 保存新状态到 sessionStorage
-        sessionStorage.setItem('feedState', JSON.stringify({
-          posts: result.data,
-          totalPages: result.totalPages,
-          pageNum: pageNum + 1,
-          newNotifNums: result.newNotifCounts,
-        }));
+        sessionStorage.setItem(
+          'feedState',
+          JSON.stringify({
+            posts: result.data,
+            totalPages: result.totalPages,
+            pageNum: pageNum + 1,
+            newNotifNums: result.newNotifCounts,
+          })
+        );
       });
       setPageNum(prevPageNum => prevPageNum + 1);
     }
+    const handleScroll = () => {
+      if (feedContentRef.current) {
+        // 记录当前滚动位置
+        const scrollPosition = feedContentRef.current!.scrollTop;
+        console.log(scrollPosition);
+        sessionStorage.setItem('scrollPosition', scrollPosition.toString());
+      }
+    };
 
+    // 如果feedContentRef.current存在，则添加事件监听器
+    const feedContentElement = feedContentRef.current;
+    if (feedContentElement) {
+      feedContentElement.addEventListener('scroll', handleScroll);
+    }
+
+    // 组件卸载时移除监听器
+    return () => {
+      if (feedContentElement) {
+        feedContentElement.removeEventListener('scroll', handleScroll);
+      }
+    };
   }, []);
-
 
   return (
     <div className={'feed-page'}>
@@ -98,7 +128,7 @@ export function Feed() {
         <span></span>
         <NewNotif newNotifNums={newNotifNums}></NewNotif>
       </Header>
-      <div className={'feed-content'} >
+      <div className={'feed-content'} ref={feedContentRef}>
         <InfiniteScroll
           initialLoad={false}
           useWindow={false}
